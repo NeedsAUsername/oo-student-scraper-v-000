84f3f39ebb3edf0bcd872dd16667f3f5c8807ac3
Kevin Chi <kevinchi118@gmail.com>
2018-07-02 01:38:47 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,7 +26,7 @@ class Scraper
           existing_attributes << "#{a.attribute("href".value)}"
       end
       existing_attributes.each do |existing_attribute|
-          if existing_attribute
+          if existing_attribute.include?("")
     #   attributes = {}
     #   doc.css(".social-icon-containter a").attribute("href")
     # #   attributes = []
